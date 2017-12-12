a0b1c1863bdafd4d486734ab500cca2cd02a0ab9
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2017-12-13 02:07:51 +0330

    init

---
+++ app/src/main/java/arashrz/dview/DLinearLayout.java
@@ -0,0 +1,17 @@
+package arashrz.dview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by arashrasoulzadeh on 12/13/2017 AD.
+ * www.mrarash.ir
+ */
+
+public class DLinearLayout extends LinearLayout {
+
+    public DLinearLayout(Context context, @android.support.annotation.Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/arashrz/dview/MainActivity.java
@@ -0,0 +1,14 @@
+package arashrz.dview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
